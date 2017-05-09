f017fedbbaafad9de1db37851d0037ea57ef65ff
Emre-Kul <emrekul35@gmail.com>
2017-05-09 21:05:16 +0300

    project page consept

--- imports/ui/js/project.js
+++ imports/ui/js/project.js
@@ -1,3 +1,8 @@
 import { Template } from "meteor/templating";
 import "../html/project.html";
- 
+
+Template.project.helpers({
+  getUserProjects(){
+    return Meteor.dbProject.getProjects({userId : Meteor.userId()});
+  },
+});
