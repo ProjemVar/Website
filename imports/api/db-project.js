93b50e7b1eb867bf12d0f299531436fffceaead2
Emre-Kul <emrekul35@gmail.com>
2017-05-13 16:10:58 +0300

    project create

--- imports/api/db-project.js
+++ imports/api/db-project.js
@@ -3,12 +3,16 @@ var ProjectCol = new Mongo.Collection("Project");
 
 Meteor.dbProject = {
   getProjects : function(name,val){
-    return ProjectCol.find({name : val});
+    return ProjectCol.find({name : val},{sort: {create_date: -1}});
+    //return ProjectCol.find();
   },
   setProject : function(id,obj){
     ProjectCol.update({_id : id},$set : obj);
   },
-  addProject : function(data){
-    ProjectCol.insert(data);
+  addProject : function(name,desc){
+      var userId = Meteor.userId();
+      if(userId == null) return;
+      var date = new Date();
+      ProjectCol.insert({UserId : userId,Name : name,Description : desc,create_date : date,update_date : date});
   },
 };
--- imports/ui/js/project.js
+++ imports/ui/js/project.js
@@ -1,8 +1,24 @@
 import { Template } from "meteor/templating";
 import "../html/project.html";
+import "../../api/db-project.js";
 
 Template.project.helpers({
   getUserProjects(){
-    return Meteor.dbProject.getProjects({userId : Meteor.userId()});
+    return Meteor.dbProject.getProjects({UserId : Meteor.userId()});
+  }
+});
+
+
+Template.project.events({
+  "click #create"(event,instance){
+    event.preventDefault();
+    var name = $("#cName").val();
+    var desc = $("#cDesc").val();
+    $("#cName").val("");
+    $("#cDesc").val("");
+    Meteor.dbProject.addProject(name,desc);
   },
+
+
+
 });
