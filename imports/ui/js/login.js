bc2bb8261677eed98175557b3eedcc99abdc77a7
pleycpl <h4rvey@protonmail.com>
2017-05-15 20:41:37 +0000

    updated login.js with airbnb js style

--- imports/ui/js/login.js
+++ imports/ui/js/login.js
@@ -1,32 +1,38 @@
-import { Template } from "meteor/templating";
-import "../html/login.html";
+import { Meteor } from 'meteor/meteor'
+import { FlowRouter } from 'meteor/kadira:flow-router'
+import { Template } from 'meteor/templating'
+import { ReactiveVar } from 'meteor/reactive-var'
+import '../html/login.html'
 
-Template.login.onCreated(function loginOnCreated() {
-	if(Meteor.userId() !== null)
-		FlowRouter.go("/home");
-	this.errorMessage = new ReactiveVar("");
-});
+Template.login.onCreated(function loginOnCreated () {
+  if (Meteor.userId() !== null) {
+    FlowRouter.go('/home')
+  }
+  this.errorMessage = new ReactiveVar('')
+})
 
 Template.login.helpers({
-	message(){
-		return Template.instance().errorMessage.get();
-	},
-	token(){
-		return Meteor.userId();
-	},
-});
+  message () {
+    return Template.instance().errorMessage.get()
+  },
+  token () {
+    return Meteor.userId()
+  }
+})
 
 Template.login.events({
-	'submit form'(event,instance){
-		event.preventDefault();
-		var username = $("[name=username]").val();
-		var password = $("[name=password]").val();
-		Meteor.loginWithPassword(username,password,function(error){
-			if(error)
-				instance.errorMessage.set(error.reason);
-			else
-				FlowRouter.go("/home");
-		});
-
-	},
-});
+  'submit form' (event, instance) {
+    event.preventDefault()
+    let username = event.target.username.value
+    let password = event.target.password.value
+    // var username = $('[name=username]').val()
+    // var password = $('[name=password]').val()
+    Meteor.loginWithPassword(username, password, function (error) {
+      if (error) {
+        instance.errorMessage.set(error.reason)
+      } else {
+        FlowRouter.go('/home')
+      }
+    })
+  }
+})
