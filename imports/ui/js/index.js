21ce63bfed73e04836d6fb68f8ffa1d2323b98f7
pleycpl <h4rvey@protonmail.com>
2017-05-15 20:40:58 +0000

    updated index.js with airbnb js style

--- imports/ui/js/index.js
+++ imports/ui/js/index.js
@@ -1,9 +1,10 @@
-import { Template } from "meteor/templating";
-import "../html/index.html";
-import "../../api/db-profile.js";
+import { Meteor } from 'meteor/meteor'
+import { Template } from 'meteor/templating'
+import '../html/index.html'
+import '../../api/db-profile.js'
 
 Template.index.helpers({
-  allProfiles(){
-    return Meteor.dbProfile.listAllProfiles();
-  },
-});
+  allProfiles () {
+    return Meteor.dbProfile.listAllProfiles()
+  }
+})
