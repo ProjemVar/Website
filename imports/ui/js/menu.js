583e8616432fe4b34372638f90c5b3550258ece5
pleycpl <h4rvey@protonmail.com>
2017-05-15 20:42:27 +0000

    updated menu.js with airbnb js style

--- imports/ui/js/menu.js
+++ imports/ui/js/menu.js
@@ -1,18 +1,19 @@
-import { Template } from "meteor/templating";
-import "../html/menu.html";
+import { Meteor } from 'meteor/meteor'
+import { FlowRouter } from 'meteor/kadira:flow-router'
+import { Template } from 'meteor/templating'
+import '../html/menu.html'
 
 Template.menu.helpers({
-  logined(){
-    return (Meteor.userId() != null);
-  },
-});
+  logined () {
+    return (Meteor.userId() != null)
+  }
+})
 
 Template.menu.events({
-  'click #logout'(event,instance){
-    event.preventDefault();
-    Meteor.logout();
-    FlowRouter.go("/");
-    //alert(Meteor.userId());
-  },
-
-});
+  'click #logout' (event, instance) {
+    event.preventDefault()
+    Meteor.logout()
+    FlowRouter.go('/')
+    // alert(Meteor.userId());
+  }
+})
