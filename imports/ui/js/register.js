2b9609c0727548b4cdb6c193ab550f575d03deee
pleycpl <h4rvey@protonmail.com>
2017-05-15 20:45:32 +0000

    updated register template

--- imports/ui/js/register.js
+++ imports/ui/js/register.js
@@ -1,42 +1,41 @@
-import { Template } from "meteor/templating";
-import "../html/register.html";
+import { Meteor } from 'meteor/meteor'
+import { Accounts } from 'meteor/accounts-base'
+import { FlowRouter } from 'meteor/kadira:flow-router'
+import { Template } from 'meteor/templating'
+import '../html/register.html'
+
 Template.register.events({
-	'submit #regform'(event,instance){
-		event.preventDefault();
-
-		var username = $("[name=regusername]").val();
-		var email = $("[name=regemail]").val();
-		var password = $("[name=regpassword]").val();
-		var repassword = $("[name=regrepassword]").val();
-
-		if(!Meteor.myAuthFuncs.comparePass(password,repassword)){
-			console.log("Pass Not Same");
-			return;
-		}
-		var passControl = Meteor.myAuthFuncs.isPasswordValid(password);
-		console.log(passControl.isErr + " -- " +passControl.msg);
-		if(passControl.isErr){
-			console.log(passControl.msg);
-			return;
-		}
-		if(!Meteor.myAuthFuncs.isEmailValid(email)){
-			console.log("Email Not Valid ");
-			return;
-		}
-		Accounts.createUser({
-				username: username,
-				email: email,
-				password: password,
-			},
-			function(error){
-				if(error)
-					console.log(error.reason);
-				else{
-					Meteor.dbProfile.createProfile();
-					FlowRouter.go("/home");
-				}
-
-			}
-		);
-	},
-});
+  'submit #regform' (event, instance) {
+    event.preventDefault()
+    let username = event.target.username.value
+    let email = event.target.email.value
+    let password = event.target.password.value
+    let repassword = event.target.repassword.value
+    if (!Meteor.myAuthFuncs.comparePass(password, repassword)) {
+      console.log('Pass Not Same')
+      return
+    }
+    var passControl = Meteor.myAuthFuncs.isPasswordValid(password)
+    console.log(passControl.isErr + ' -- ' + passControl.msg)
+    if (passControl.isErr) {
+      console.log(passControl.msg)
+      return
+    }
+    if (!Meteor.myAuthFuncs.isEmailValid(email)) {
+      console.log('Email Not Valid')
+      return
+    }
+    Accounts.createUser({
+      username: username,
+      email: email,
+      password: password
+    }, function (error) {
+      if (error) {
+        console.log(error.reason)
+      } else {
+        Meteor.dbProfile.createProfile()
+        FlowRouter.go('/home')
+      }
+    })
+  }
+})
