3e6f904504e148bae26544ac00f8e86977c36ab2
pleycpl <h4rvey@protonmail.com>
2017-05-15 14:19:40 +0000

    codes updated with Airbnb Style Guide

--- imports/api/db-profile.js
+++ imports/api/db-profile.js
@@ -1,34 +1,34 @@
-import { Mongo } from "meteor/mongo";
-var ProfileCol = new Mongo.Collection("Profile");
+import { Mongo } from 'meteor/mongo'
+import { Meteor } from 'meteor/meteor'
+var ProfileCol = new Mongo.Collection('Profile')
 
 Meteor.dbProfile = {
-  listAllProfiles : function(){
-    return ProfileCol.find();
+  listAllProfiles: function () {
+    return ProfileCol.find()
   },
-  getProfile : function(){
-    var userId = Meteor.userId();
-    if(userId == null)
-    {
-      console.log("getProfile NULL");
-      return null;
+  getProfile: function () {
+    var userId = Meteor.userId()
+    if (userId == null) {
+      console.log('getProfile NULL')
+      return null
     }
-    return ProfileCol.findOne({UserId:userId});
+    return ProfileCol.findOne({UserId: userId})
   },
-  createProfile : function(name="",surname=""){
-      var userId = Meteor.userId();
-      if(userId == null){
-        console.log("createProfile NULL");
-        return;
-      }
-      ProfileCol.insert({UserId : userId,Name : name,Surname : surname});
-  },
-  setProfile : function(name,surname){
-    var user = Meteor.dbProfile.getProfile();
-    if(user == null){
-      console.log("setProfile Null");
-      this.createProfile(name,surname);
-      return;
+  createProfile: function (name = '', surname = '') {
+    var userId = Meteor.userId()
+    if (userId == null) {
+      console.log('createProfile NULL')
+      return
     }
-    ProfileCol.update({_id: user._id},{$set : {Name : name,Surname : surname}});
+    ProfileCol.insert({UserId: userId, Name: name, Surname: surname})
   },
-};
+  setProfile: function (name, surname) {
+    var user = Meteor.dbProfile.getProfile()
+    if (user == null) {
+      console.log('setProfile Null')
+      this.createProfile(name, surname)
+      return
+    }
+    ProfileCol.update({_id: user._id}, {$set: {Name: name, Surname: surname}})
+  }
+}
