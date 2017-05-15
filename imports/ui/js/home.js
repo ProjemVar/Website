4ee0431668593b14eedfb62fb1a83715c47bd6aa
pleycpl <h4rvey@protonmail.com>
2017-05-15 20:40:19 +0000

    updated home.js with airbnb js style

--- imports/ui/js/home.js
+++ imports/ui/js/home.js
@@ -1,7 +1,10 @@
-import { Template } from "meteor/templating";
-import "../html/home.html";
+import { Meteor } from 'meteor/meteor'
+import { FlowRouter } from 'meteor/kadira:flow-router'
+import { Template } from 'meteor/templating'
+import '../html/home.html'
 
-Template.home.onCreated(function homeOnCreated(){
-  if(Meteor.userId() == null)
-		FlowRouter.go("/");
-});
+Template.home.onCreated(function homeOnCreated () {
+  if (Meteor.userId() == null) {
+    FlowRouter.go('/')
+  }
+})
