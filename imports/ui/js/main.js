2bbb87f19c19873b73e5eaedb675ad5138776244
pleycpl <h4rvey@protonmail.com>
2017-05-15 20:42:45 +0000

    updated main.js

--- imports/ui/js/main.js
+++ imports/ui/js/main.js
@@ -1,11 +1,8 @@
-import { Template } from "meteor/templating";
-
-import "../html/main.html";
-
-import "./index.js";
-import "./project.js";
-import "./profile.js";
-import "./register.js";
-import "./login.js";
-import "./home.js";
-import "./menu.js";
+import '../html/main.html'
+import './index.js'
+import './project.js'
+import './profile.js'
+import './register.js'
+import './login.js'
+import './home.js'
+import './menu.js'
