e2dfd7de18f39c699bc48387399022dd801635a6
pleycpl <h4rvey@protonmail.com>
2017-05-16 19:26:34 +0000

    updated project template

--- imports/ui/js/project.js
+++ imports/ui/js/project.js
@@ -1,24 +1,37 @@
 import { Meteor } from 'meteor/meteor'
 import { Template } from 'meteor/templating'
+import { Bert } from 'meteor/themeteorchef:bert'
+import { Projects } from '../../../lib/collections/collections.js'
 import '../html/project.html'
-import '../../api/db-project.js'
 
 Template.project.helpers({
   getUserProjects () {
-    return Meteor.dbProject.getProjectsByUserId()
+    console.log(Meteor.userId())
+    return Projects.find({userId: Meteor.userId()}, {sort: {createdAt: -1}})
+  },
+  getAllProjects () {
+    return Projects.find({}, {sort: {createdAt: -1}})
   }
 })
 
 Template.project.events({
-  'click #create' (event, instance) {
-    event.preventDefault()
-    let name = event.target.cName.value
-    let desc = event.target.cDesc.value
-    let content = event.target.cContent.value
-    Meteor.dbProject.addProject(name, desc, content)
-  },
-  'click #deleteProject' (event, instance) {
-    Meteor.dbProject.deleteProject(this._id)
-    // alert(this._id);
+  'submit .project-post': function (event) {
+    let projectName = Meteor.myAuthFuncs.trimInput(event.target.projectName.value)
+    let projectDesc = Meteor.myAuthFuncs.trimInput(event.target.projectDesc.value)
+    let projectContent = Meteor.myAuthFuncs.trimInput(event.target.projectContent.value)
+    console.log(projectName, projectDesc, projectContent)
+    if (Meteor.myAuthFuncs.isNotEmpty(projectName) &&
+        Meteor.myAuthFuncs.isNotEmpty(projectDesc) &&
+        Meteor.myAuthFuncs.isNotEmpty(projectContent)) {
+      // do stuff
+      Meteor.call('addProjects', projectName, projectDesc, projectContent)
+      event.target.projectName.value = ''
+      event.target.projectDesc.value = ''
+      event.target.projectContent.value = ''
+      Bert.alert('Your Project Was Posted!', 'success', 'growl-top-right')
+    } else {
+      Bert.alert('something went wrong', 'danger', 'growl-top-right')
+    }
+    return false // Prevent submit
   }
 })
