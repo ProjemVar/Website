77f2ccb6877a5009b4b294255b1297813b92a6f8
pleycpl <h4rvey@protonmail.com>
2017-05-16 11:24:39 +0000

    updated menu template, logout events

--- imports/ui/js/menu.js
+++ imports/ui/js/menu.js
@@ -1,15 +1,21 @@
 import { Meteor } from 'meteor/meteor'
 import { FlowRouter } from 'meteor/kadira:flow-router'
 import { Template } from 'meteor/templating'
+import { Bert } from 'meteor/themeteorchef:bert'
 import '../html/menu.html'
 
 Template.menu.helpers({
 })
 
 Template.menu.events({
-  'click #logout' (event, instance) {
-    event.preventDefault()
-    Meteor.logout()
-    FlowRouter.go('/')
+  'click #logout': function (event) {
+    Meteor.logout(function (err) {
+      if (err) {
+        Bert.alert(err.reason, 'danger', 'growl-top-right')
+      } else {
+        FlowRouter.go('/')
+        Bert.alert('You Are Now Logged Out', 'success', 'growl-top-right')
+      }
+    })
   }
 })
