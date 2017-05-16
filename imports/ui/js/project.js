6051a790c5e588c8353eb3df5a0e1079f39d0f7b
pleycpl <h4rvey@protonmail.com>
2017-05-16 22:06:44 +0000

    added updating project and showing project's information

--- imports/ui/js/project.js
+++ imports/ui/js/project.js
@@ -45,12 +45,42 @@ Template.showproject.helpers({
   getId: function () {
     var id = FlowRouter.getParam('id')
     return id
+  },
+  project: function () {
+    var id = FlowRouter.getParam('id')
+    return Projects.findOne({_id: id})
   }
 })
 
+// editproject template
 Template.editproject.helpers({
   getId: function () {
     var id = FlowRouter.getParam('id')
     return id
+  },
+  project: function () {
+    var id = FlowRouter.getParam('id')
+    return Projects.findOne({_id: id})
+  }
+})
+
+Template.editproject.events({
+  'submit .project-update-post': function (event) {
+    let projectName = Meteor.myAuthFuncs.trimInput(event.target.projectName.value)
+    let projectDesc = Meteor.myAuthFuncs.trimInput(event.target.projectDesc.value)
+    let projectContent = Meteor.myAuthFuncs.trimInput(event.target.projectContent.value)
+    let id = FlowRouter.getParam('id')
+    console.log(projectName, projectDesc, projectContent)
+    if (Meteor.myAuthFuncs.isNotEmpty(projectName) &&
+        Meteor.myAuthFuncs.isNotEmpty(projectDesc) &&
+        Meteor.myAuthFuncs.isNotEmpty(projectContent)) {
+      // do stuff
+      Meteor.call('updatedProject', id, projectName, projectDesc, projectContent)
+      Bert.alert('Your Project Was Updated!', 'success', 'growl-top-right')
+      FlowRouter.go('/project/' + id)
+    } else {
+      Bert.alert('something went wrong', 'danger', 'growl-top-right')
+    }
+    return false // Prevent submit
   }
 })
