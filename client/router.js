1dd9c5d3439b16d32bf1dd16db5413e5ac544b5d
pleycpl <h4rvey@protonmail.com>
2017-05-16 20:24:30 +0000

    updated router, and passing params to templates

--- client/router.js
+++ client/router.js
@@ -1,53 +1,58 @@
-import { Template } from 'meteor/templating';
-import { ReactiveVar } from 'meteor/reactive-var';
-
-import '../imports/ui/js/main.js';
-
+import { FlowRouter } from 'meteor/kadira:flow-router'
+import { BlazeLayout } from 'meteor/kadira:blaze-layout'
+import '../imports/ui/js/main.js'
 
 FlowRouter.route('/', {
-  action: function() {
-    //BlazeLayout.render("main", {content : "index",topmenu : "indexmenu"});
-    BlazeLayout.render("main", {content : "index"});
+  action: function () {
+    // BlazeLayout.render("main", {content : "index",topmenu : "indexmenu"});
+    BlazeLayout.render('main', {content: 'index'})
   }
-});
-
-FlowRouter.route('/profile/', {
-  action: function() {
-    BlazeLayout.render("main", {content : "profile"});
-  }
-});
-///Project begin
-FlowRouter.route('/project/', {
-  action: function() {
-    BlazeLayout.render("main", {content : "project"});
-  }
-});
-
-FlowRouter.route('/project/:id', {
-  action: function() {
-    BlazeLayout.render("main", {content : "showproject"});
-  }
-});
-FlowRouter.route('/project/edit/:id', {
-  action: function() {
-    BlazeLayout.render("main", {content : "editproject"});
-  }
-});
-//project end
-FlowRouter.route('/login/', {
-  action: function() {
-    BlazeLayout.render("main", {content : "login"});
-  }
-});
-
-FlowRouter.route('/register/', {
-  action: function() {
-    BlazeLayout.render("main", {content : "register"});
-  }
-});
+})
 
 FlowRouter.route('/home/', {
-  action: function() {
-    BlazeLayout.render("main", {content : "home"});
+  action: function () {
+    BlazeLayout.render('main', {content: 'home'})
   }
-});
+})
+
+FlowRouter.route('/profile/', {
+  action: function () {
+    BlazeLayout.render('main', {content: 'profile'})
+  }
+})
+
+FlowRouter.route('/login/', {
+  action: function () {
+    BlazeLayout.render('main', {content: 'login'})
+  }
+})
+
+FlowRouter.route('/register/', {
+  action: function () {
+    BlazeLayout.render('main', {content: 'register'})
+  }
+})
+// Project begin
+FlowRouter.route('/project/', {
+  action: function () {
+    BlazeLayout.render('main', {content: 'project'})
+  }
+})
+
+FlowRouter.route('/project/:id', {
+  action: function (params, queryParams) {
+    BlazeLayout.render('main', {
+      content: 'showproject',
+      params: params
+    })
+  }
+})
+FlowRouter.route('/project/edit/:id', {
+  action: function (params, queryParams) {
+    BlazeLayout.render('main', {
+      content: 'editproject',
+      params: params
+    })
+  }
+})
+// project end
