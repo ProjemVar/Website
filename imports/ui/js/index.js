54eaaa56e2490f7b4345285b2df1cc7b3578f43b
pleycpl <h4rvey@protonmail.com>
2017-05-17 14:06:47 +0000

    added delete-user event and controlling auth

--- imports/ui/js/index.js
+++ imports/ui/js/index.js
@@ -1,12 +1,27 @@
 import { Meteor } from 'meteor/meteor'
 import { Template } from 'meteor/templating'
+import { Bert } from 'meteor/themeteorchef:bert'
 import '../html/index.html'
 
 Template.index.helpers({
-  allProfiles () {
+  isAdmin () {
+    var username = Meteor.user().username
+    if (username === 'admin') {
+      return true
+    }
+    return false
+  },
+  allUsers () {
     return Meteor.users.find({})
   },
   userEmail: function () {
     return this.emails[0].address
+  }
+})
+
+Template.index.events({
+  'click #delete-user': function (event) {
+    Meteor.call('removeUser', this._id)
+    Bert.alert('User Was Deleted', 'success', 'growl-top-right')
   }
 })
