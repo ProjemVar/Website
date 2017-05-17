e2a3ca1428b45372924bd6ce191172145d353ecb
pleycpl <h4rvey@protonmail.com>
2017-05-17 20:59:46 +0000

    updated home template

--- imports/ui/js/home.js
+++ imports/ui/js/home.js
@@ -1,10 +1,6 @@
-import { Meteor } from 'meteor/meteor'
-import { FlowRouter } from 'meteor/kadira:flow-router'
 import { Template } from 'meteor/templating'
 import '../html/home.html'
 
 Template.home.onCreated(function homeOnCreated () {
-  if (Meteor.userId() == null) {
-    FlowRouter.go('/')
-  }
+  console.log('Home Page created')
 })
