52ed9b41ce327778de6a202a95c0a126c3e69b1c
pleycpl <h4rvey@protonmail.com>
2017-05-17 11:10:50 +0000

    updated editprofile template and it's event

--- imports/ui/js/profile.js
+++ imports/ui/js/profile.js
@@ -1,4 +1,5 @@
 import { Meteor } from 'meteor/meteor'
+import { Accounts } from 'meteor/accounts-base'
 import { FlowRouter } from 'meteor/kadira:flow-router'
 import { Template } from 'meteor/templating'
 import { Bert } from 'meteor/themeteorchef:bert'
@@ -37,32 +38,30 @@ Template.editprofile.events({
     event.preventDefault()
     let username = Meteor.myAuthFuncs.trimInput(event.target.username.value)
     let email = Meteor.myAuthFuncs.trimInput(event.target.email.value)
-    let oldpassword = Meteor.myAuthFuncs.trimInput(event.target.oldpassword.value)
+    let oldPassword = Meteor.myAuthFuncs.trimInput(event.target.oldpassword.value)
     let password = Meteor.myAuthFuncs.trimInput(event.target.password.value)
     let repassword = Meteor.myAuthFuncs.trimInput(event.target.repassword.value)
     let id = FlowRouter.getParam('id')
     if (Meteor.myAuthFuncs.isNotEmpty(username) &&
         Meteor.myAuthFuncs.isNotEmpty(email) &&
-        isCorrectPass(id, oldpassword) &&
+        Meteor.myAuthFuncs.isNotEmpty(oldPassword) &&
         Meteor.myAuthFuncs.isNotEmpty(password) &&
+        Meteor.myAuthFuncs.isNotEmpty(repassword) &&
         Meteor.myAuthFuncs.isEmail(email) &&
         Meteor.myAuthFuncs.areValidPasswords(password, repassword)) {
       // do stuff
-      Meteor.call('updatedProfile', id, username, email, password)
-      Bert.alert('Your Profile Was Updated!', 'success', 'growl-top-right')
-      FlowRouter.go('/profile/' + id)
+      Accounts.changePassword(oldPassword, password, function (error) {
+        if (error) {
+          Bert.alert('Not changing password', 'danger', 'growl-top-right')
+        } else {
+          Meteor.call('updatedProfile', id, username, email, password)
+          Bert.alert('Your Profile Was Updated!', 'success', 'growl-top-right')
+          FlowRouter.go('/profile/' + id)
+        }
+      })
     } else {
       Bert.alert('something went wrong', 'danger', 'growl-top-right')
     }
     return false // Prevent submit
   }
 })
-
-let isCorrectPass = function (userid, password) {
-  if (Meteor.users.find({_id: userid}).password === password) {
-    console.log(true)
-    return true
-  }
-  console.log(false)
-  return false
-}
