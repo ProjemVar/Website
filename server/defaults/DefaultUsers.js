4ca3c5290ab2cca6619bc722e2802243015f4e0e
pleycpl <h4rvey@protonmail.com>
2017-05-18 10:58:03 +0000

    modified emrekul user and added mustafa user

--- server/defaults/DefaultUsers.js
+++ server/defaults/DefaultUsers.js
@@ -1,4 +1,5 @@
 export const DefaultUsers = [
   {username: 'admin', email: 'admin@admin.com', password: '123456', profile: {}},
-  {username: 'emrekul', email: 'emrekul@gmail.com', passowd: '123456', profile: {}}
+  {username: 'emrekul', email: 'emrekul@gmail.com', password: '123456', profile: {}},
+  {username: 'mustafa', email: 'mustafa@gmail.com', password: '123456', profile: {}}
 ]
