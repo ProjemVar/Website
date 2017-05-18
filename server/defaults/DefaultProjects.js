292ba54ab12ca2fbf8cd991cc8f8c993bae735f1
pleycpl <h4rvey@protonmail.com>
2017-05-18 10:15:35 +0000

    added some users and projects

--- server/defaults/DefaultProjects.js
+++ server/defaults/DefaultProjects.js
@@ -1,3 +1,5 @@
 export const DefaultProjects = [
-  {Author: 'admin', Name: 'Hadi Yapalim', Desc: 'Ne yapicaz?', Content: 'Kafamiza gore takilalim.'}
+  {Author: 'admin', Name: 'Hadi Yapalim', Desc: 'Ne yapicaz?', Content: 'Kafamiza gore takilalim.'},
+  {Author: 'admin', Name: 'Atom', Desc: 'Editor yapalim?', Content: 'Meteorjs ile localhost e calisan text editor'},
+  {Author: 'emrekul', Name: 'ProjemVar', Desc: 'Projelerin paylasilacagi ortam', Content: 'Projelerin yapilmasi icin bulusulan topluluk.'}
 ]
--- server/defaults/DefaultUsers.js
+++ server/defaults/DefaultUsers.js
@@ -1,3 +1,4 @@
 export const DefaultUsers = [
-  {username: 'admin', email: 'admin@admin.com', password: '123456', profile: {}}
+  {username: 'admin', email: 'admin@admin.com', password: '123456', profile: {}},
+  {username: 'emrekul', email: 'emrekul@gmail.com', passowd: '123456', profile: {}}
 ]
