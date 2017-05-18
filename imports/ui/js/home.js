d053fbb218a7c433b8241df9fc80724570a6a5ac
pleycpl <h4rvey@protonmail.com>
2017-05-18 10:28:06 +0000

    updated home template

--- imports/ui/js/home.js
+++ imports/ui/js/home.js
@@ -1,6 +1,13 @@
+import { Meteor } from 'meteor/meteor'
 import { Template } from 'meteor/templating'
 import '../html/home.html'
 
 Template.home.onCreated(function homeOnCreated () {
   console.log('Home Page created')
+})
+
+Template.home.helpers({
+  getUsername: function () {
+    return Meteor.user().username
+  }
 })
