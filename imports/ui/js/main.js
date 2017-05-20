91f65a5c581ee5a0e4a5cecbcf399da98f31f58d
pleycpl <h4rvey@protonmail.com>
2017-05-20 16:35:21 +0000

    updated with seperated template name

--- imports/ui/js/main.js
+++ imports/ui/js/main.js
@@ -3,12 +3,15 @@ import { Template } from 'meteor/templating'
 import { Projects } from '../../../lib/collections/collections.js'
 import '../html/main.html'
 import './index.js'
-import './project.js'
-import './profile.js'
 import './register.js'
 import './login.js'
 import './home.js'
 import './menu.js'
+import './projectEdit.js'
+import './projectShow.js'
+import './projects.js'
+import './profileEdit.js'
+import './profile.js'
 
 Template.main.helpers({
   countProject: function () {
