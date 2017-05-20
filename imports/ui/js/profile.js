579c1967f984db34567113fc5c66f7b4577406a1
pleycpl <h4rvey@protonmail.com>
2017-05-20 16:33:13 +0000

    seperated templates

--- imports/ui/js/profile.js
+++ imports/ui/js/profile.js
@@ -1,12 +1,11 @@
 import { Meteor } from 'meteor/meteor'
-import { Accounts } from 'meteor/accounts-base'
 import { FlowRouter } from 'meteor/kadira:flow-router'
 import { Template } from 'meteor/templating'
 import { Bert } from 'meteor/themeteorchef:bert'
 import { Projects } from '../../../lib/collections/collections.js'
 import '../html/profile.html'
 
-Template.profileWithId.helpers({
+Template.profile.helpers({
   getUserProjects () {
     let id = FlowRouter.getParam('id')
     console.log(id)
@@ -32,61 +31,9 @@ Template.profileWithId.helpers({
     return false
   }
 })
-Template.profileWithId.events({})
-
-Template.editprofile.helpers({
-  getId: function () {
-    let id = FlowRouter.getParam('id')
-    return id
-  },
-  profile: function () {
-    let id = FlowRouter.getParam('id')
-    let user = Meteor.users.findOne({_id: id})
-    return {
-      username: user.username,
-      email: user.emails[0].address
-    }
-  },
-  isAdminOrYourself () {
-    var username = Meteor.user().username
-    let id = FlowRouter.getParam('id')
-    let user = Meteor.users.findOne({_id: id})
-    if (username === 'admin' || username === user.username) {
-      return true
-    }
-    return false
-  }
-})
-
-Template.editprofile.events({
-  'submit .profile-update-post': function (event) {
-    event.preventDefault()
-    let username = Meteor.myAuthFuncs.trimInput(event.target.username.value)
-    let email = Meteor.myAuthFuncs.trimInput(event.target.email.value)
-    let oldPassword = Meteor.myAuthFuncs.trimInput(event.target.oldpassword.value)
-    let password = Meteor.myAuthFuncs.trimInput(event.target.password.value)
-    let repassword = Meteor.myAuthFuncs.trimInput(event.target.repassword.value)
-    let id = FlowRouter.getParam('id')
-    if (Meteor.myAuthFuncs.isNotEmpty(username) &&
-        Meteor.myAuthFuncs.isNotEmpty(email) &&
-        Meteor.myAuthFuncs.isNotEmpty(oldPassword) &&
-        Meteor.myAuthFuncs.isNotEmpty(password) &&
-        Meteor.myAuthFuncs.isNotEmpty(repassword) &&
-        Meteor.myAuthFuncs.isEmail(email) &&
-        Meteor.myAuthFuncs.areValidPasswords(password, repassword)) {
-      // do stuff
-      Accounts.changePassword(oldPassword, password, function (error) {
-        if (error) {
-          Bert.alert('Not changing password', 'danger', 'growl-top-right')
-        } else {
-          Meteor.call('updatedProfile', id, username, email, password)
-          Bert.alert('Your Profile Was Updated!', 'success', 'growl-top-right')
-          FlowRouter.go('/profile/' + id)
-        }
-      })
-    } else {
-      Bert.alert('something went wrong', 'danger', 'growl-top-right')
-    }
-    return false // Prevent submit
+Template.profile.events({
+  'click #delete-project': function (event) {
+    Meteor.call('removeProject', this._id)
+    Bert.alert('Your Project Was Deleted', 'success', 'growl-top-right')
   }
 })
