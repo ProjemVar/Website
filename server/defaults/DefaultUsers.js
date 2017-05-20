481be14e161a263e0c38749d40d2c81e46615921
pleycpl <h4rvey@protonmail.com>
2017-05-20 13:31:46 +0000

    modified for user schema

--- server/defaults/DefaultUsers.js
+++ server/defaults/DefaultUsers.js
@@ -1,5 +1,5 @@
 export const DefaultUsers = [
-  {username: 'admin', email: 'admin@admin.com', password: '123456', profile: {}},
-  {username: 'emrekul', email: 'emrekul@gmail.com', password: '123456', profile: {}},
-  {username: 'mustafa', email: 'mustafa@gmail.com', password: '123456', profile: {}}
+  {username: 'admin', email: 'admin@admin.com', password: '123456', scores: {}},
+  {username: 'emrekul', email: 'emrekul@gmail.com', password: '123456', scores: {}},
+  {username: 'mustafa', email: 'mustafa@gmail.com', password: '123456', scores: {}}
 ]
