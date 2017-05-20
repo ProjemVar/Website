0478b22229bc5eb6f354b508654bfa856e9d2414
Emre-Kul <emrekul35@gmail.com>
2017-05-21 00:07:52 +0300

    search system added

--- imports/ui/js/pages/PageHome.js
+++ imports/ui/js/pages/PageHome.js
@@ -1,17 +1,35 @@
 import { Meteor } from 'meteor/meteor'
 import { Template } from 'meteor/templating'
+import { ReactiveVar } from 'meteor/reactive-var'
 import { Projects } from '../../../../lib/collections/collections.js'
 import '../../html/pages/PageHome.html'
 
 Template.PageHome.onCreated(function homeOnCreated () {
-  console.log('Home Page created')
+  this.searchQuery = new ReactiveVar("");
 })
 
 Template.PageHome.helpers({
   getUsername: function () {
     return Meteor.user().username
   },
-  getBestProjects: function () {
+  getBestorSearchedProjects: function () {
+    let searchQ = Template.instance().searchQuery.get()
+    if(searchQ.length > 0)
+      return Projects.find(
+        {
+          $or : [
+            {projectDesc : {$regex : searchQ, $options : 'i'}},
+            {projectName : {$regex : searchQ, $options : 'i'}}
+          ]
+        },
+        { sort: {'totalScore': -1} }
+      )
     return Projects.find({}, { sort: {'totalScore': -1} })
+  }
+})
+Template.PageHome.events({
+  'click #search-project' : function(event,instance){
+    let searchQ = $("#search-project-query").val()
+    instance.searchQuery.set(searchQ)
   }
 })
