513897461623a52db9f66e72f857a535236c763f
pleycpl <h4rvey@protonmail.com>
2017-05-21 11:41:13 +0000

    showing date with momentjs

--- imports/ui/js/pages/PageChat.js
+++ imports/ui/js/pages/PageChat.js
@@ -2,12 +2,16 @@ import { Meteor } from 'meteor/meteor'
 import { Template } from 'meteor/templating'
 import { FlowRouter } from 'meteor/kadira:flow-router'
 import { Messages } from '../../../../lib/collections/collections.js'
+import moment from 'moment'
 import '../../html/pages/PageChat.html'
 
 Template.messages.helpers({
   messages: function () {
     let id = FlowRouter.getParam('id')
     return Messages.find({projectId: id}, {sort: {time: 1}})
+  },
+  getDate: function (time) {
+    return moment(time).format('MMMM Do YYYY, h:mm:ss a')
   }
 })
 
