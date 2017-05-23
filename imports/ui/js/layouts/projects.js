303fa69550364f16f7d3bb9dda8ea7467fa2dfca
pleycpl <h4rvey@protonmail.com>
2017-05-23 20:13:08 +0000

    checking page last

--- imports/ui/js/layouts/projects.js
+++ imports/ui/js/layouts/projects.js
@@ -8,24 +8,30 @@ Template.projects.onCreated(function () {
   // our ReactiveVar to it, making it accessible throughout the
   // current template instance.
   this.page = new ReactiveVar(0)
-  console.log('this.page: ', this.page)
+  this.pageLast = new ReactiveVar(false)
 })
 
 Template.projects.helpers({
   getAllProjects () {
-    console.log('Runned getAllProjects helpers')
     console.log(Projects.find({}, {sort: {createdAt: -1}}))
     console.log('Page: ', Template.instance().page.get())
-    return Projects.find({}, {sort: {createdAt: -1}, skip: Template.instance().page.get() * 5, limit: 5})
+    let projects = Projects.find({}, {sort: {createdAt: -1}, skip: Template.instance().page.get() * 5, limit: 5})
+    if (!projects.count()) Template.instance().pageLast.set(true)
+    return projects
   }
 })
 Template.projects.events({
   'click #next': function (event) {
-    let page = Template.instance().page
-    page.set(page.get() + 1)
+    if (!Template.instance().pageLast.get()) {
+      let page = Template.instance().page
+      page.set(page.get() + 1)
+    }
   },
   'click #prev': function (event) {
     let page = Template.instance().page
-    page.set(page.get() - 1)
+    if (page.get() > 0) {
+      page.set(page.get() - 1)
+    }
+    Template.instance().pageLast.set(false)
   }
 })
